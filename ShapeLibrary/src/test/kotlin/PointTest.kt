eea81fca981c8f6f3c38f73cdbc8976036f66658
Melanie Pena <penamelanie3@gmail.com>
2022-05-13 14:59:24 -0600

    added tests for Point.clone and Point.move

--- ShapeLibrary/src/test/kotlin/PointTest.kt
+++ ShapeLibrary/src/test/kotlin/PointTest.kt
@@ -31,5 +31,49 @@ internal class PointTest {
         assertTrue(point.y == testY)
     }
 
+    @Test
+    fun testClone() {
+        val testX = -1.0
+        val testY = 5.8
+        val point: Point = Point(testX, testY)
+        val pointClone = point.clone()
+        assertTrue(pointClone.x == point.x)
+        assertTrue(pointClone.y == point.y)
+    }
 
+    @Test
+    fun testMoveZero() {
+        val testX = 1.0
+        val testY = 1.0
+        val point: Point = Point(testX, testY)
+        point.move(0.0, 0.0)
+        assertTrue(point.x == testX)
+        assertTrue(point.y == testY)
+    }
+
+    @Test
+    fun testMovePositive() {
+        val testX = 1.0
+        val testY = 1.0
+        val deltaX = 5.0
+        val deltaY = 10.5
+        val point: Point = Point(testX, testY)
+
+        point.move(deltaX, deltaY)
+        assertTrue(point.x == testX + deltaX)
+        assertTrue(point.y == testY + deltaY)
+    }
+
+    @Test
+    fun testMoveNegative() {
+        val testX = 1.0
+        val testY = 1.0
+        val deltaX = -5.0
+        val deltaY = -10.5
+        val point: Point = Point(testX, testY)
+
+        point.move(deltaX, deltaY)
+        assertTrue(point.x == testX + deltaX)
+        assertTrue(point.y == testY + deltaY)
+    }
 }

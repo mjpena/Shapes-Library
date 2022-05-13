43f7922ef0cc4b9b29528baef8beaf0163cf1dbd
Melanie Pena <penamelanie3@gmail.com>
2022-05-13 14:26:17 -0600

    created initial Point class with necessary attributes and behaviors

---
+++ ShapeLibrary/src/main/kotlin/Point.kt
@@ -0,0 +1,27 @@
+/**
+ * @property x position of point object on x axis
+ * @property y position of point object on y axis
+ * @constructor creates Point object given x and y
+ */
+class Point(x:Double, y:Double){
+    private var x:Double = x
+    private var y:Double = y
+
+    /**
+     * Creates new point object with the same attributes of this object.
+     * @return Cloned Point object.
+     */
+    fun clone(): Point{
+        return Point(x, y)
+    }
+
+    /**
+     * Moves point according to deltaX and deltaY.
+     * @param deltaX Amount to move point on x axis.
+     * @param deltaY Amount to move point on y axis.
+     */
+    fun move(deltaX: Double, deltaY: Double){
+        x += deltaX
+        y += deltaY
+    }
+}

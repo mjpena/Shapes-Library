de873b6889dd1c9e1ad4d71f4b25fd4c0a61adff
Melanie Pena <penamelanie3@gmail.com>
2022-05-14 21:07:00 -0600

    added Triangle class and tests

--- ShapeLibrary/src/main/kotlin/Triangle.kt
+++ ShapeLibrary/src/main/kotlin/Triangle.kt
@@ -1,2 +1,27 @@
-class Triangle {
+import kotlin.math.sqrt
+
+/**
+ * @param point1 First point used to define triangle.
+ * @param point2 Second point used to define triangle.
+ * @param point3 Third point used to define triangle.
+ */
+class Triangle(point1: Point, point2: Point, point3: Point): Shape(listOf(point1, point2, point3))
+{
+    init
+    {
+        if (this.getArea() == 0.0)
+        {
+            throw Exception("Triangle cannot have area of 0.")
+        }
+    }
+
+    override fun getArea(): Double
+    {
+        val length1 = Line(points[0], points[1]).getLength()
+        val length2 = Line(points[0], points[2]).getLength()
+        val length3 = Line(points[1], points[2]).getLength()
+        val semiPerimeter = (length1 + length2 + length3) / 2
+        return sqrt(semiPerimeter * (semiPerimeter-length1) * (semiPerimeter-length2) * (semiPerimeter-length3))
+    }
+
 }
--- ShapeLibrary/src/test/kotlin/TriangleTest.kt
+++ ShapeLibrary/src/test/kotlin/TriangleTest.kt
@@ -5,6 +5,68 @@ import org.junit.jupiter.api.Assertions.*
 internal class TriangleTest {
 
     @Test
-    fun getArea() {
+    fun getAttributes()
+    {
+        val point1: Point = Point(0.0, 0.0)
+        val point2: Point = Point(0.0, 3.0)
+        val point3: Point = Point(3.0, 3.0)
+        val triangle: Triangle = Triangle(point1, point2, point3)
+        assertTrue(triangle.points[0] == point1)
+        assertTrue(triangle.points[1] == point2)
+        assertTrue(triangle.points[2] == point3)
+    }
+
+    @Test
+    fun areaZero()
+    {
+        val point1: Point = Point(0.0, 0.0)
+        val point2: Point = Point(0.0, 3.0)
+        val point3: Point = Point(0.0, 5.0)
+        org.junit.jupiter.api.assertThrows<Exception>()
+        {
+            val triangle: Triangle = Triangle(point1, point2, point3)
+        }
+    }
+
+    @Test
+    fun getAreaPositive()
+    {
+        val point1: Point = Point(0.0, 0.0)
+        val point2: Point = Point(2.0, 0.0)
+        val point3: Point = Point(2.0, 2.0)
+        val triangle: Triangle = Triangle(point1, point2, point3)
+        val area: Double  = 2.0 * 2.0 * 0.5
+        val area2: Double = triangle.getArea()
+        assertEquals(area, area2, .000001)
+    }
+
+    @Test
+    fun getAreaNegative()
+    {
+        val point1: Point = Point(0.0, 0.0)
+        val point2: Point = Point(-2.0, 0.0)
+        val point3: Point = Point(-2.0, -2.0)
+        val triangle: Triangle = Triangle(point1, point2, point3)
+        val area: Double  = 2.0 * 2.0 * 0.5
+        val area2: Double = triangle.getArea()
+        assertEquals(area, area2, .000001)
+    }
+
+    @Test
+    fun testMove()
+    {
+        val point1: Point = Point(0.0, 0.0)
+        val point2: Point = Point(2.0, 0.0)
+        val point3: Point = Point(2.0, 2.0)
+        val triangle: Triangle = Triangle(point1, point2, point3)
+        val deltaX: Double = 5.3
+        val deltaY: Double = -41.3
+        triangle.move(deltaX, deltaY)
+        assertTrue(triangle.points[0].x == 5.3)
+        assertTrue(triangle.points[0].y == -41.3)
+        assertTrue(triangle.points[1].x == 7.3)
+        assertTrue(triangle.points[1].y == -41.3)
+        assertTrue(triangle.points[2].x == 7.3)
+        assertTrue(triangle.points[2].y == -39.3)
     }
 }
